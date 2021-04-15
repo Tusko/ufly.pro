d34b624732d6eacbacd69a89a4e8576d846090a9
Tusko <tusko@photoinside.me>
2021-04-15 17:56:39 +0300

    migrate old design to vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div id="wrap" class="animatedParent" data-sequence="500">
+    <div class="valign">
+      <img
+        data-id="1"
+        id="logo"
+        src="./assets/logo.png"
+        alt="uFly Video Production"
+        class="animated bounceInDown"
+      />
+      <h1 data-id="2" class="animated growIn hidden">uFly.pro</h1>
+      <h2 data-id="3" class="animated fadeInUpShort">Flying professionals</h2>
+    </div>
+    <div class="vfix"></div>
+  </div>
+  <article>
+    <h3>аерозйомка</h3>
+    <h3>львів</h3>
+    <h3>аеро фото</h3>
+    <h3>aero</h3>
+    <h3>aerofilming</h3>
+    <h3>aerowedding</h3>
+    <h3>аерозймока весілля</h3>
+    <h3>аеро в оренду</h3>
+    <h3>повітряна зйомка Львів</h3>
+    <h3>зйомка дронами у Львові</h3>
+    <h3>drone</h3>
+    <h3>flying</h3>
+    <h3>Lviv</h3>
+    <h3>Ukraine</h3>
+    <h3>Video Production</h3>
+  </article>
+  <footer class="animatedParent delay-1000">
+    <div class="animated flipInX">
+      <a
+        href="https://instagram.com/ufly.pro/"
+        class="icon-insta"
+        target="_blank"
+      ></a>
+      <a href="https://vk.com/uflypro" class="icon-vk" target="_blank"></a>
+    </div>
+  </footer>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style src="./assets/styles.scss" lang="scss">
 </style>
